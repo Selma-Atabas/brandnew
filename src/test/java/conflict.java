acad9d7a0aff5577f1a6acb8ccfa0d3ce4a5cea8
Selma-Atabas <68811734+Selma-Atabas@users.noreply.github.com>
2020-10-15 22:22:07 +0200

    Update conflict.java

--- src/test/java/conflict.java
+++ src/test/java/conflict.java
@@ -1,6 +1,8 @@
 public class conflict {
 
     public static void main(String[] args) {
-        System.out.println("this is practice to solve conflict");
+        System.out.println("this is practice to solve (understand) conflict");
+        
+        
     }
 }
