d4d75997c899d07e3ca78b92bd07c6d04c02feb3
Selma-Atabas <selma-aykal43@hotmail.com>
2020-10-15 22:22:56 +0200

    solve conflict stuff

--- src/test/java/conflict.java
+++ src/test/java/conflict.java
@@ -2,5 +2,7 @@ public class conflict {
 
     public static void main(String[] args) {
         System.out.println("this is practice to solve conflict");
+
+        System.out.println("Sill realy awesome hello");
     }
 }
